ac909516036c39254db51668e5993fc3eab20de2
Stevie <xxlennygamerxx@gmail.com>
2021-08-28 17:00:18 -0700

    Some fixes and WebUtilities.

--- Geometric/Data/Raw/RawGDLevel.cs
+++ Geometric/Data/Raw/RawGDLevel.cs
@@ -49,17 +49,17 @@ namespace Geometric.Data.Raw
         /// <summary>
         ///     Number of downloads.
         /// </summary>
-        [JsonPropertyName("downloads")] public string Downloads;
+        [JsonPropertyName("downloads")] public int Downloads;
 
         /// <summary>
         ///     Number of likes.
         /// </summary>
-        [JsonPropertyName("likes")] public string Likes;
+        [JsonPropertyName("likes")] public int Likes;
 
         /// <summary>
         ///     If the level has a negative number of likes.
         /// </summary>
-        [JsonPropertyName("disliked")] public string Disliked;
+        [JsonPropertyName("disliked")] public bool Disliked;
 
         /// <summary>
         ///     The length of the level (Tiny, Short, Medium, Long, or XL).
@@ -69,27 +69,27 @@ namespace Geometric.Data.Raw
         /// <summary>
         ///     Amount of stars received for beating the level.
         /// </summary>
-        [JsonPropertyName("stars")] public string Stars;
+        [JsonPropertyName("stars")] public int Stars;
 
         /// <summary>
         ///     Amount of mana orbs received for beating the level.
         /// </summary>
-        [JsonPropertyName("orbs")] public string Orbs;
+        [JsonPropertyName("orbs")] public int Orbs;
 
         /// <summary>
         ///     Amount of diamonds received for beating the level.
         /// </summary>
-        [JsonPropertyName("diamonds")] public string Diamonds;
+        [JsonPropertyName("diamonds")] public int Diamonds;
 
         /// <summary>
         ///     Whether the level is featured or not.
         /// </summary>
-        [JsonPropertyName("featured")] public string Featured;
+        [JsonPropertyName("featured")] public bool Featured;
 
         /// <summary>
         ///     Whether the level has an "epic" rating or not.
         /// </summary>
-        [JsonPropertyName("epic")] public string Epic;
+        [JsonPropertyName("epic")] public bool Epic;
 
         /// <summary>
         ///     The version of Geometry Dash the level was released on (1.9, 2.1, etc.).
@@ -99,7 +99,7 @@ namespace Geometric.Data.Raw
         /// <summary>
         ///     Number of times the level was updated.
         /// </summary>
-        [JsonPropertyName("version")] public string Version;
+        [JsonPropertyName("version")] public int Version;
 
         /// <summary>
         ///     The original level ID, if the level was copied. Otherwise returns 0.
@@ -109,47 +109,47 @@ namespace Geometric.Data.Raw
         /// <summary>
         ///     If the level has two player mode enabled.
         /// </summary>
-        [JsonPropertyName("twoPlayer")] public string TwoPlayer;
+        [JsonPropertyName("twoPlayer")] public bool TwoPlayer;
 
         /// <summary>
         ///     The level number of the song, if no custom song is used. Otherwise returns 0.
         /// </summary>
-        [JsonPropertyName("officialSong")] public string OfficialSong;
+        [JsonPropertyName("officialSong")] public int OfficialSong;
 
         /// <summary>
         ///     The Id of the song, if a custom song was used. Otherwise returns 0.
         /// </summary>
-        [JsonPropertyName("customSong")] public string CustomSong;
+        [JsonPropertyName("customSong")] public int CustomSong;
 
         /// <summary>
         ///     Number of user coins placed in the level.
         /// </summary>
-        [JsonPropertyName("coins")] public string Coins;
+        [JsonPropertyName("coins")] public int Coins;
 
         /// <summary>
         ///     Whether these coins are verified or not.
         /// </summary>
-        [JsonPropertyName("verifiedCoins")] public string VerifiedCoins;
+        [JsonPropertyName("verifiedCoins")] public bool VerifiedCoins;
 
         /// <summary>
         ///     How many stars the author requested the level to be rated. 0 if no request was given.
         /// </summary>
-        [JsonPropertyName("starsRequested")] public string StarsRequested;
+        [JsonPropertyName("starsRequested")] public int StarsRequested;
 
         /// <summary>
         ///     The number of objects in the level. This was added in a recent version of Geometry Dash, so older levels will simply return 0.
         /// </summary>
-        [JsonPropertyName("objects")] public string Objects;
+        [JsonPropertyName("objects")] public uint Objects;
 
         /// <summary>
         ///     Whether the level is considered "large" (more than 40k objects).
         /// </summary>
-        [JsonPropertyName("large")] public string Large;
+        [JsonPropertyName("large")] public bool Large;
 
         /// <summary>
         ///     How many creator points the level is worth (1 for star rating, 1 for feature, and 1 for epic rating).
         /// </summary>
-        [JsonPropertyName("cp")] public string Cp;
+        [JsonPropertyName("cp")] public int Cp;
 
         // ReSharper disable once CommentTypo
         /// <summary>
@@ -178,7 +178,7 @@ namespace Geometric.Data.Raw
         /// <summary>
         ///     The ID of the song (again). If a non-custom song was used, this will return a level number of the song.
         /// </summary>
-        [JsonPropertyName("songID")] public string SongId;
+        [JsonPropertyName("songID")] public int SongId;
 
         /// <summary>
         ///     The link to the raw MP3 of the song, if available.
@@ -209,18 +209,46 @@ namespace Geometric.Data.Raw
         /// <summary>
         ///     The amount of seconds spent in the editor (currently only works when using GDBrowser locally).
         /// </summary>
-        [JsonPropertyName("editorTime")] public string EditorTime;
+        [JsonPropertyName("editorTime")] public ulong EditorTime;
 
         /// <summary>
         ///     The amount of seconds spent in the editor, including time from the level it was copied from.
         /// </summary>
-        [JsonPropertyName("totalEditorTime")] public string TotalEditorTime;
-        [JsonPropertyName("ldm")] public string Ldm;
-        [JsonPropertyName("weekly")] public string Weekly;
-        [JsonPropertyName("dailyNumber")] public string DailyNumber;
-        [JsonPropertyName("nextDaily")] public string NextDaily;
+        [JsonPropertyName("totalEditorTime")] public ulong TotalEditorTime;
+
+        /// <summary>
+        ///     If the level contains a checkbox for Low Detail Mode.
+        /// </summary>
+        [JsonPropertyName("ldm")] public bool Ldm;
+
+        /// <summary>
+        ///     If the values below represent the weekly demon rather than the daily level.
+        /// </summary>
+        [JsonPropertyName("weekly")] public bool Weekly;
+
+        /// <summary>
+        ///     Which daily/weekly the level is (e.g. 1,000th daily level).
+        /// </summary>
+        [JsonPropertyName("dailyNumber")] public int DailyNumber;
+
+        /// <summary>
+        ///     The amount of seconds until the daily/weekly level expires.
+        /// </summary>
+        [JsonPropertyName("nextDaily")] public ulong NextDaily;
+
+        /// <summary>
+        ///     The Unix timestamp for when the daily/weekly level expires.
+        /// </summary>
         [JsonPropertyName("nextDailyTimestamp")] public string NextDailyTimestamp;
+
+        /// <summary>
+        ///     An unknown data string.
+        /// </summary>
         [JsonPropertyName("extraString")] public string ExtraString;
+
+        /// <summary>
+        ///     The actual data of the level, compressed with GZIP.
+        /// </summary>
         [JsonPropertyName("data")] public string Data;
     }
 }
---
+++ Geometric/Web/WebUtilities.cs
@@ -0,0 +1,102 @@
+﻿#region License
+// Copyright (C) 2021 Tomat and Contributors, MIT License
+#endregion
+
+namespace Geometric.Web
+{
+    /// <summary>
+    ///     Web-related utilities.
+    /// </summary>
+    public static class WebUtilities
+    {
+        /// <summary>
+        ///     <c>/</c>. Used for valid URL joining.
+        /// </summary>
+        public const char SeparatorCharacter = '/';
+
+        /// <summary>
+        ///     Base API link.
+        /// </summary>
+        public const string ApiLink = "https://gdbrowser.com/api";
+
+        /// <summary>
+        ///     Joins strings together using <see cref="SeparatorCharacter"/> for valid URL creation.
+        /// </summary>
+        /// <param name="strings">The strings to join together.</param>
+        /// <returns>A single string joined by <see cref="SeparatorCharacter"/>.</returns>
+        public static string CombineUrlValid(params string[] strings) => string.Join(SeparatorCharacter, strings);
+
+        /// <summary>
+        ///     Appends the given <paramref name="strings"/> to <see cref="ApiLink"/> using <see cref="CombineUrlValid"/>.
+        /// </summary>
+        /// <param name="strings">The strings to append together.</param>
+        /// <returns>A single string beginning with <see cref="ApiLink"/>, along with the <paramref name="strings"/> joined together with <see cref="SeparatorCharacter"/>.</returns>
+        public static string AppendToApi(params string[] strings) => CombineUrlValid(ApiLink, CombineUrlValid(strings));
+
+        /// <summary>
+        ///     Gets a valid level URL based on the level.
+        /// </summary>
+        public static string GetLevelUrl(string level) => AppendToApi("level", level);
+
+        /// <summary>
+        ///     Gets a valid profile URL based on the profile.
+        /// </summary>
+        /// <remarks>
+        ///     Accepts either an account's username or its ID.
+        /// </remarks>
+        public static string GetProfile(string user) => AppendToApi("level", user);
+
+        /// <summary>
+        ///     Gets a valid search URL.
+        /// </summary>
+        public static string GetSearch(string query) => AppendToApi("search", query);
+        
+        /// <summary>
+        ///     Gets the leaderboard URL.
+        /// </summary>
+        public static string GetLeaderboard() => AppendToApi("leaderboard");
+
+        /// <summary>
+        ///     Gets a valid level leaderboard URL.
+        /// </summary>
+        public static string GetLeaderboardLevel(string level) => AppendToApi("leaderboardLevel", level);
+
+        /// <summary>
+        ///     Gets a valid comment list URL.
+        /// </summary>
+        /// <remarks>
+        ///     Accepts either an account's username or its ID.
+        /// </remarks>
+        public static string GetComments(string user) => AppendToApi("comments", user);
+
+        /// <summary>
+        ///     Gets a valid level analysis URL.
+        /// </summary>
+        public static string GetLevelAnalysis(string level) => AppendToApi("analyze", level);
+
+        /// <summary>
+        ///     Gets a valid commenting URL.
+        /// </summary>
+        public static string GetCommenting() => AppendToApi("postComment");
+
+        /// <summary>
+        ///     Gets a valid profile commenting URL.
+        /// </summary>
+        public static string GetProfileCommenting() => AppendToApi("postProfileComment");
+
+        /// <summary>
+        ///     Gets a valid messages URL.
+        /// </summary>
+        public static string GetMessages() => AppendToApi("messages");
+
+        /// <summary>
+        ///     Gets a valid liking URL.
+        /// </summary>
+        public static string GetLiking() => AppendToApi("like");
+
+        /// <summary>
+        ///     Gets a valid user icon request URL.
+        /// </summary>
+        public static string GetIcon(string user) => AppendToApi("icon", user);
+    }
+}
